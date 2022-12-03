2eaacfc1b678f70aa58ea5313e8e06cc7a97c21d
dimasolyshko <dmitriy.solyshko@mail.ru>
2022-12-03 22:01:32 +0300

    TCP

---
+++ Приложение на сокетах/Socket/Client/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ClientTCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool end = true;
+            while (end)
+                {
+                const string ip = "127.0.0.1";
+                const int port = 8080;
+                var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+                var tcpSoket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); // подключение сокета
+
+                Console.WriteLine("Введите сообщение!");
+                var message = Console.ReadLine(); // переменная для сообщения
+                if (message == "exit") end = false;
+
+                var data = Encoding.UTF8.GetBytes(message);// перевели сообщение в байты
+                tcpSoket.Connect(tcpEndPoint);
+                tcpSoket.Send(data);
+                // Получение сообщения от сервера!
+                var buffer = new byte[256];
+                var size = 0; // количество байт
+                var answer = new StringBuilder();
+                do
+                {
+                    size = tcpSoket.Receive(buffer); // Записываем количество реально полученных байт
+                    answer.Append(Encoding.UTF8.GetString(buffer, 0, size)); // буфер это откуда берём информацию, 0 это начало, size - количество байт
+                }
+                while (tcpSoket.Available > 0); // до тех пор пока есть данные
+
+                Console.WriteLine(answer.ToString());
+                tcpSoket.Shutdown(SocketShutdown.Both);
+                tcpSoket.Close(); // закрытие клиента
+            }
+        }
+    }
+}
---
+++ Приложение на сокетах/Socket/Server/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerTCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string ip = "127.0.0.1";
+            const int port = 8080;
+
+            var tcpEndPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+
+            var tcpSoket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp); // подключение сокета
+
+            tcpSoket.Bind(tcpEndPoint); // Прослушивание
+            tcpSoket.Listen(5);
+
+            while (true)
+            {
+                var Listener = tcpSoket.Accept(); // новый слушатель для каждого нового клиента
+                var buffer = new byte[256];
+                var size = 0; // количество байт
+                var data = new StringBuilder();
+                do
+                {
+                    size = Listener.Receive(buffer); // Записываем количество реально полученных байт
+                    data.Append(Encoding.UTF8.GetString(buffer, 0, size)); // буфер это откуда берём информацию, 0 это начало, size - количество байт
+
+                }
+                while (Listener.Available > 0); // до тех пор пока есть данные
+
+                Console.WriteLine(data); // проверка строки
+                Listener.Send(Encoding.UTF8.GetBytes("Всё хорошо! Сообщение отправлено!"));
+                Listener.Shutdown(SocketShutdown.Both);
+                Listener.Close(); // закрытие клиента
+            }
+        }
+    }
+    class database
+    {
+        string name;
+        int age;
+    }
+}
