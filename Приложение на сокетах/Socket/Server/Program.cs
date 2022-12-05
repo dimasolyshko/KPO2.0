c7f8c814f40268dd514ac817185de5d531289f63
dimasolyshko <dmitriy.solyshko@mail.ru>
2022-12-05 16:47:20 +0300

    Добавлен класс database

--- Приложение на сокетах/Socket/Server/Program.cs
+++ Приложение на сокетах/Socket/Server/Program.cs
@@ -10,6 +10,28 @@ namespace ServerTCP
     {
         static void Main(string[] args)
         {
+            void ReadInfoAboutDataBase(ref database [] Db, ref int row)
+            {
+                string path = "DataBase.txt";   // путь к файлу
+                // чтение из файла
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string[] words;
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        Array.Resize(ref Db, ++row);
+                        words = line.Split(' ');
+                        Db[row - 1] = new database(words[0],Convert.ToInt32(words[1]));                   
+                    }
+                }
+                for (int i = 0; i < row; i++) Db[i].Print();
+            }
+            int row = 0;
+            database[] Db = new database[row];
+            ReadInfoAboutDataBase(ref Db, ref row);
+            Console.WriteLine(row);
+
             const string ip = "127.0.0.1";
             const int port = 8080;
 
@@ -30,13 +52,10 @@ namespace ServerTCP
                 {
                     size = Listener.Receive(buffer); // Записываем количество реально полученных байт
                     data.Append(Encoding.UTF8.GetString(buffer, 0, size)); // буфер это откуда берём информацию, 0 это начало, size - количество байт
-
                 }
                 while (Listener.Available > 0); // до тех пор пока есть данные
 
                 Console.WriteLine(data); // проверка строки
-                int row = 0;
-                database[] Db = new database[row];
                 Listener.Send(Encoding.UTF8.GetBytes("Всё хорошо! Сообщение отправлено!"));
                 Listener.Shutdown(SocketShutdown.Both);
                 Listener.Close(); // закрытие клиента
@@ -46,7 +65,21 @@ namespace ServerTCP
     }
     class database
     {
-        string name;
-        int age;
+        public string name;
+        public int age;
+        public database()
+        {
+            age = 0;
+            name = "Неизвестно";
+        }
+        public database(string name, int age)
+        {
+            this.age = age;
+            this.name = name;
+        }
+        public void Print()
+        {
+            Console.WriteLine($"{name} {age}");
+        }
     }
 }
